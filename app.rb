b557db90058736156b7d8d3069ea8cb64af97112
Leora Soibelman <21lsoibelman@gannacademy.org>
2018-07-18 13:27:43 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,8 +1,17 @@
 class App < Sinatra::Base
-
-	get '/' do
-		erb :index
-	end
-
-
+get '/' do
+"Words"
+end 
+get '/hello' do
+  erb :hello
+end 
+get '/goodbye' do
+  erb :goodbye
+end 
+get '/date' do
+  erb :date
+end 
+get '/testing' do
+  erb :testing
+end 
 end
